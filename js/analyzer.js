9ecd3c33e175c73c3dfca001baa37e000ea5f449
Joseph Osborn <jcosborn@ucsc.edu>
2014-07-20 13:33:46 -0700

    Skeleton web worker for long-running analyses.

--- js/analyzer.js
+++ js/analyzer.js
@@ -12,6 +12,20 @@ if(!this.hasOwnProperty("compileAndAnalyze") ||
 	compile = compileAndAnalyze;
 }
 
+//Launch a web worker to do analysis without blocking the UI.
+var worker = new Worker("js/analyzer/worker.js");
+
+worker.onmessage = function(msg) {
+	console.log("got "+JSON.stringify(msg.data)+" from worker");
+}
+
+worker.onerror = function(event) {
+    throw new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
+}
+
+//worker.postMessage({type:"start"});
+//worker.postMessage({type:"stop"});
+
 function analyze(command,text,randomseed) {
 	//by this time, compile has already been called.
 	console.log("analyze "+command+" with "+randomseed);

c0e9a603cb77c4e1a45da153ef3ebfbcf9d26b8a
Joseph Osborn <jcosborn@ucsc.edu>
2014-08-25 07:06:07 -0700

    Guard hint highlighting so that hints are only highlighted when the level is solved. Turn spaces into tabs.

--- js/analyzer.js
+++ js/analyzer.js
@@ -41,8 +41,8 @@ var Analyzer = (function() {
 	var gameRules = "";
 	var levelQueue = [];
 	var seenSolutions = {};
-    var lastSeenSolutions = {};
-    
+	var lastSeenSolutions = {};
+	
 	var lineHighlights = {};
 	var solvedClass = "line-solved";
 	var unsolvedClass = "line-unsolved";
@@ -56,21 +56,21 @@ var Analyzer = (function() {
 	var workerScripts = {
 		"solve": "js/analyzer/worker_solve.js"
 	};
-    
-    module.clear = function clear() {
-        clearLineHighlights();
-        lastRules = "";
-        gameRules = "";
-        levelQueue = [];
-        seenSolutions = {};
-        lastSeenSolutions = {};
-        for(var t in workerLookup) {
-            for(var k in workerLookup) {
-                killWorker(t, k);
-            }
-        }
-        workers = [];
-    }
+	
+	module.clear = function clear() {
+		clearLineHighlights();
+		lastRules = "";
+		gameRules = "";
+		levelQueue = [];
+		seenSolutions = {};
+		lastSeenSolutions = {};
+		for(var t in workerLookup) {
+			for(var k in workerLookup) {
+				killWorker(t, k);
+			}
+		}
+		workers = [];
+	}
 
 	//Launch a web worker to do analysis without blocking the UI.
 	module.analyze = function analyze(command,text,randomseed) {
@@ -131,65 +131,65 @@ var Analyzer = (function() {
 		}
 		return editor.lineCount();
 	}
-    
-    function arrayEquiv(a1, a2) {
-        if(a1.length != a2.length) { return false; }
-        for(var i = 0; i < a1.length; i++) {
-            if(a1[i] != a2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+	
+	function arrayEquiv(a1, a2) {
+		if(a1.length != a2.length) { return false; }
+		for(var i = 0; i < a1.length; i++) {
+			if(a1[i] != a2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    function memberArray(arrays, array) {
-        for(var i = 0; i < arrays.length; i++) {
-            var ar = arrays[i];
-            if(arrayEquiv(ar, array)) {
-                return ar;
-            }
-        }
-        return null;
-    }
-    
-    function hasPrefix(array, prefix) {
-        if(prefix.length > array.length) { return false; }
-        for(var i = 0; i < prefix.length; i++) {
-            if(array[i] != prefix[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    function anyHasPrefix(arrays, prefix) {
-        for(var i = 0; i < arrays.length; i++) {
-            var ar = arrays[i];
-            if(hasPrefix(ar, prefix)) {
-                return ar;
-            }
-        }
-        return null;
-    }
+	function memberArray(arrays, array) {
+		for(var i = 0; i < arrays.length; i++) {
+			var ar = arrays[i];
+			if(arrayEquiv(ar, array)) {
+				return ar;
+			}
+		}
+		return null;
+	}
+	
+	function hasPrefix(array, prefix) {
+		if(prefix.length > array.length) { return false; }
+		for(var i = 0; i < prefix.length; i++) {
+			if(array[i] != prefix[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	function anyHasPrefix(arrays, prefix) {
+		for(var i = 0; i < arrays.length; i++) {
+			var ar = arrays[i];
+			if(hasPrefix(ar, prefix)) {
+				return ar;
+			}
+		}
+		return null;
+	}
 
-    function hasAnyPrefix(array, prefixes) {
-        for(var i = 0; i < prefixes.length; i++) {
-            var prefix = prefixes[i];
-            if(hasPrefix(array, prefix)) {
-                return prefix;
-            }
-        }
-        return null;
-    }
-    
-    function clearLineHighlights() {
+	function hasAnyPrefix(array, prefixes) {
+		for(var i = 0; i < prefixes.length; i++) {
+			var prefix = prefixes[i];
+			if(hasPrefix(array, prefix)) {
+				return prefix;
+			}
+		}
+		return null;
+	}
+	
+	function clearLineHighlights() {
 		for(var l in lineHighlights) {
 			editor.removeLineClass(l, "background", lineHighlights[l]);
 		}
-    }
+	}
 	
 	function updateLevelHighlights() {
-        clearLineHighlights();
+		clearLineHighlights();
 		lineHighlights = {};
 		for(var i=0; i < state.levels.length; i++) {
 			if(state.levels[i].lineNumber && seenSolutions[i]) {
@@ -204,18 +204,20 @@ var Analyzer = (function() {
 					editor.addLineClass(l, "background", solClass);
 					lineHighlights[l] = solClass;
 				}
-                //now, highlight hints:
-                //if a hint is prefix to the solution, highlight it green
-                //otherwise, highlight it red
-                var hints = hintLinesBetween(state.levels[i].lineNumber, nextLevelLine(i));
-                for(var j = 0; j < hints.lines.length; j++) {
-                    l = hints.lines[j];
-                    var hintClass = anyHasPrefix(seenSolutions[i].prefixes, hints.prefixes[j]) ? hintUsedClass : hintUnusedClass;
-                    editor.removeLineClass(l, "background", hintUsedClass);
-                    editor.removeLineClass(l, "background", hintUnusedClass);
-                    editor.addLineClass(l, "background", hintClass);
-                    lineHighlights[l] = hintClass;
-                }
+				//now, highlight hints:
+				//if a hint is prefix to the solution, highlight it green
+				//otherwise, highlight it red
+				if(seenSolutions[i].solved) {
+					var hints = hintLinesBetween(state.levels[i].lineNumber, nextLevelLine(i));
+					for(var j = 0; j < hints.lines.length; j++) {
+						l = hints.lines[j];
+						var hintClass = anyHasPrefix(seenSolutions[i].prefixes, hints.prefixes[j]) ? hintUsedClass : hintUnusedClass;
+						editor.removeLineClass(l, "background", hintUsedClass);
+						editor.removeLineClass(l, "background", hintUnusedClass);
+						editor.addLineClass(l, "background", hintClass);
+						lineHighlights[l] = hintClass;
+					}
+				}
 			}
 		}
 	}
@@ -274,7 +276,7 @@ var Analyzer = (function() {
 	
 	function hintLinesBetween(l1, l2) {
 		var hints = [];
-        var lines = [];
+		var lines = [];
 		for(var l = l1; l < l2; l++) {
 			var line = editor.getLine(l).trim();
 			var match = /\(\s*@HINT:\s*((?:UP|DOWN|LEFT|RIGHT|ACTION|WAIT)(?:\s+(UP|DOWN|LEFT|RIGHT|ACTION|WAIT))*)\s*\)/i.exec(line);
@@ -287,7 +289,7 @@ var Analyzer = (function() {
 					}
 				}
 				hints.push(hint);
-                lines.push(l);
+				lines.push(l);
 			}
 		}
 		//console.log("Using hints "+hints.map(prefixToSolutionSteps).join("<br/>&nbsp;&nbsp;"));
@@ -306,17 +308,17 @@ var Analyzer = (function() {
 		var lev = levelQueue.shift();
 		var level = state.levels[lev];
 		var hint = levelHint(lev);
-        //If we previously had some good solutions for lev:
+		//If we previously had some good solutions for lev:
 		if(seenSolutions[lev] && !seenSolutions[lev].stale) {
-            //Copy them and mark seenSolutions[lev] as stale
-            seenSolutions[lev].stale = true; 
-            lastSeenSolutions[lev] = deepClone(seenSolutions[lev]);
-        }
+			//Copy them and mark seenSolutions[lev] as stale
+			seenSolutions[lev].stale = true; 
+			lastSeenSolutions[lev] = deepClone(seenSolutions[lev]);
+		}
 		if(USE_WORKERS) {
 			startWorker("solve", lev, {
 				rules:gameRules,
 				level:lev,
-                mode:"fast",//"fast_then_best",
+				mode:"fast",//"fast_then_best",
 				//seed:randomseed,
 				hint:hint,
 				verbose:true
@@ -325,7 +327,7 @@ var Analyzer = (function() {
 			Solver.startSearch({
 				rules:gameRules,
 				level:lev,
-                mode:"fast",//"fast_then_best",
+				mode:"fast",//"fast_then_best",
 				hint:hint,
 				//seed:randomseed,
 				verbose:true,
@@ -370,10 +372,10 @@ var Analyzer = (function() {
 				}
 				recordSolution(workers[id].init.rules, workers[id].init.levelText, data);
 				consoleCacheDump();
-                // TODO: I just got a solution. was it the first solution? if so, do not continue this solver until other guys get a chance to run.
-                //     do fancy stuff with hints, warnings, etc
-                // otherwise: compare the new solution to the old best solution. let it continue normally right away.
-                //     do the same fancy stuff with hints, warnings, etc
+				// TODO: I just got a solution. was it the first solution? if so, do not continue this solver until other guys get a chance to run.
+				//	   do fancy stuff with hints, warnings, etc
+				// otherwise: compare the new solution to the old best solution. let it continue normally right away.
+				//	   do the same fancy stuff with hints, warnings, etc
 				break;
 			case "exhausted":
 				consolePrint("Level "+data.level+": Did not find more solutions after "+data.iterations+" iterations ("+data.time+" seconds)");
@@ -381,85 +383,85 @@ var Analyzer = (function() {
 					recordFailure(workers[id].init.rules, workers[id].init.levelText, data);
 					if(data.queueLength > 0) {
 						levelQueue.push(data.level);
-    					consolePrint("Level "+data.level+" is taking some time to solve.");
+						consolePrint("Level "+data.level+" is taking some time to solve.");
 					} else {
-    					consolePrint("<span class='line-level-unsolvable'>Level "+data.level+" is not solvable!</span>");
+						consolePrint("<span class='line-level-unsolvable'>Level "+data.level+" is not solvable!</span>");
 					}
 				}
-                if(lastSeenSolutions[data.level]) {
-                    var oldSolutions = lastSeenSolutions[data.level].prefixes;
-                    var newSolutions = seenSolutions[data.level].prefixes;
-                    if(lastSeenSolutions[data.level].solved && seenSolutions[data.level].solved) {
-                        var absentOldSolutions = oldSolutions.filter(function(sol) {
-                            return !memberArray(newSolutions, sol);
-                        });
-                        var brandNewSolutions = newSolutions.filter(function(sol) {
-                            return !memberArray(oldSolutions, sol);
-                        });
-                        //Print console info for solutions which have changed or disappeared.
-                        for(var i = 0; i < absentOldSolutions.length; i++) {
-                            var oldSoln = absentOldSolutions[i];
-                            var newSoln;
-                            if((newSoln = anyHasPrefix(newSolutions, oldSoln))) {
-                                consolePrint(
-                                    '<span class="line-solution-got-longer">Level '+data.level+': Solution got longer in new version:<br/>'+
-                                    '&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
-                                    '&nbsp;--&gt;<br/>'+
-                                    '&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+
-                                    '</span>'
-                                );
-                                brandNewSolutions.splice(brandNewSolutions.indexOf(newSoln),1);
-                            } else if((newSoln = hasAnyPrefix(oldSoln, newSolutions))) {
-                                consolePrint(
-                                    '<span class="line-solution-got-shorter">Level '+data.level+': Solution got shorter in new version:<br/>'+
-                                    '&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
-                                    '&nbsp;--&gt;<br/>'+
-                                    '&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+
-                                    '</span>'
-                                );
-                                brandNewSolutions.splice(brandNewSolutions.indexOf(newSoln),1);
-                            } else {
-                                consolePrint(
-                                    '<span class="line-solution-disappeared">Level '+data.level+': Solution no longer used in new version:<br/>'+
-                                    '&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
-                                    '&nbsp;--&gt;<br/>'+
-                                    '&nbsp;XXXX'+
-                                    '</span>'
-                                );
-                            }
-                        }
-                        //for each soln of newSolutions which do not share prefix
-                        //with anything in oldSolutions... (those have been removed)
-                        for(i = 0; i < brandNewSolutions.length; i++) {
-                            var newSoln = brandNewSolutions[i];
-                            consolePrint(
-                                '<span class="line-solution-appeared">Level '+data.level+': New solution appeared in new version:<br/>'+
-                                '&nbsp;XXXX'+
-                                '&nbsp;--&gt;<br/>'+
-                                '&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+'<br/>'+
-                                '</span>'
-                            );
-                        }
-                    } else if(lastSeenSolutions[data.level].solved) {
-                        //level used to be solvable but now is not
-                        consolePrint(
-                            '<span class="line-level-unsolvable">Level '+data.level+' used to be solvable by:<br/>'+
-                            '&nbsp;'+oldSolutions.map(prefixToSolutionSteps).join("<br/>&nbsp;")+'<br/>'+
-                            '</span>'
-                        );
-                    } else if(seenSolutions[data.level].solved) {
-                        consolePrint(
-                            '<span class="line-level-solvable">Level '+data.level+' is now solvable by:<br/>'+
-                            '&nbsp;'+newSolutions.map(prefixToSolutionSteps).join("<br/>&nbsp;")+'<br/>'+
-                            '</span>'
-                        );
-                    }
-                }
+				if(lastSeenSolutions[data.level]) {
+					var oldSolutions = lastSeenSolutions[data.level].prefixes;
+					var newSolutions = seenSolutions[data.level].prefixes;
+					if(lastSeenSolutions[data.level].solved && seenSolutions[data.level].solved) {
+						var absentOldSolutions = oldSolutions.filter(function(sol) {
+							return !memberArray(newSolutions, sol);
+						});
+						var brandNewSolutions = newSolutions.filter(function(sol) {
+							return !memberArray(oldSolutions, sol);
+						});
+						//Print console info for solutions which have changed or disappeared.
+						for(var i = 0; i < absentOldSolutions.length; i++) {
+							var oldSoln = absentOldSolutions[i];
+							var newSoln;
+							if((newSoln = anyHasPrefix(newSolutions, oldSoln))) {
+								consolePrint(
+									'<span class="line-solution-got-longer">Level '+data.level+': Solution got longer in new version:<br/>'+
+									'&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
+									'&nbsp;--&gt;<br/>'+
+									'&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+
+									'</span>'
+								);
+								brandNewSolutions.splice(brandNewSolutions.indexOf(newSoln),1);
+							} else if((newSoln = hasAnyPrefix(oldSoln, newSolutions))) {
+								consolePrint(
+									'<span class="line-solution-got-shorter">Level '+data.level+': Solution got shorter in new version:<br/>'+
+									'&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
+									'&nbsp;--&gt;<br/>'+
+									'&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+
+									'</span>'
+								);
+								brandNewSolutions.splice(brandNewSolutions.indexOf(newSoln),1);
+							} else {
+								consolePrint(
+									'<span class="line-solution-disappeared">Level '+data.level+': Solution no longer used in new version:<br/>'+
+									'&nbsp;'+prefixToSolutionSteps(oldSoln).join(" ")+'<br/>'+
+									'&nbsp;--&gt;<br/>'+
+									'&nbsp;XXXX'+
+									'</span>'
+								);
+							}
+						}
+						//for each soln of newSolutions which do not share prefix
+						//with anything in oldSolutions... (those have been removed)
+						for(i = 0; i < brandNewSolutions.length; i++) {
+							var newSoln = brandNewSolutions[i];
+							consolePrint(
+								'<span class="line-solution-appeared">Level '+data.level+': New solution appeared in new version:<br/>'+
+								'&nbsp;XXXX'+
+								'&nbsp;--&gt;<br/>'+
+								'&nbsp;'+prefixToSolutionSteps(newSoln).join(" ")+'<br/>'+
+								'</span>'
+							);
+						}
+					} else if(lastSeenSolutions[data.level].solved) {
+						//level used to be solvable but now is not
+						consolePrint(
+							'<span class="line-level-unsolvable">Level '+data.level+' used to be solvable by:<br/>'+
+							'&nbsp;'+oldSolutions.map(prefixToSolutionSteps).join("<br/>&nbsp;")+'<br/>'+
+							'</span>'
+						);
+					} else if(seenSolutions[data.level].solved) {
+						consolePrint(
+							'<span class="line-level-solvable">Level '+data.level+' is now solvable by:<br/>'+
+							'&nbsp;'+newSolutions.map(prefixToSolutionSteps).join("<br/>&nbsp;")+'<br/>'+
+							'</span>'
+						);
+					}
+				}
 				consoleCacheDump();
 				break;
 			case "hintInsufficient":
-                //TODO: avoid showing this message for system-provided (i.e. non-user) hints.
-                if(seenSolutions[data.level] && !seenSolutions[data.level].solved) { break; }
+				//TODO: avoid showing this message for system-provided (i.e. non-user) hints.
+				if(seenSolutions[data.level] && !seenSolutions[data.level].solved) { break; }
 				consolePrint("<span class='line-insufficient-hints'>Level "+data.level+": Hints did not solve level on their own.</span>");
 				break;
 			default:
@@ -467,28 +469,28 @@ var Analyzer = (function() {
 		}
 	}
 	
-    //Add a solution to seenSolutions[lev], unless it's stale in which case clobber it and wait for more.
+	//Add a solution to seenSolutions[lev], unless it's stale in which case clobber it and wait for more.
 	function recordSolution(ruleText, levelText, data) {
 		var level = data.level;
 		var soln = data.solution;
-        if(!seenSolutions[level] || seenSolutions[level].stale) {
-    		seenSolutions[level] = {
-    			ruleText:ruleText,
-    			levelText:levelText,
-    			level:state.levels[level],
-    			solved:true,
-    			stale:false,
-    			prefixes:soln.prefixes,
-    			steps:soln.prefixes.map(prefixToSolutionSteps),
-    			iteration:data.iteration,
-    			exhaustive:data.queueLength == 0,
-    			f:soln.f, g:soln.g, h:soln.h
-    		};
-        } else {
-            seenSolutions[level].prefixes = seenSolutions[level].prefixes.concat(soln.prefixes);
-            seenSolutions[level].steps = seenSolutions[level].steps.concat(soln.prefixes.map(prefixToSolutionSteps));
-    		seenSolutions[level].exhaustive = data.queueLength == 0;
-        }
+		if(!seenSolutions[level] || seenSolutions[level].stale) {
+			seenSolutions[level] = {
+				ruleText:ruleText,
+				levelText:levelText,
+				level:state.levels[level],
+				solved:true,
+				stale:false,
+				prefixes:soln.prefixes,
+				steps:soln.prefixes.map(prefixToSolutionSteps),
+				iteration:data.iteration,
+				exhaustive:data.queueLength == 0,
+				f:soln.f, g:soln.g, h:soln.h
+			};
+		} else {
+			seenSolutions[level].prefixes = seenSolutions[level].prefixes.concat(soln.prefixes);
+			seenSolutions[level].steps = seenSolutions[level].steps.concat(soln.prefixes.map(prefixToSolutionSteps));
+			seenSolutions[level].exhaustive = data.queueLength == 0;
+		}
 		
 		updateLevelHighlights();
 		return seenSolutions[level];
@@ -595,10 +597,10 @@ var Analyzer = (function() {
 		}
 
 		w.postMessage({type:"start",
-			           id:workers.length,
-			           workerType:wtype,
-			           key:key,
-			           init:init});
+					   id:workers.length,
+					   workerType:wtype,
+					   key:key,
+					   init:init});
 		log("sent init message");
 		workers.push(w);
 
